db6a2d4f622e54fdd17e8f29703c3579df71913a
Claudia <claudia.vidya@student.umn.ac.id>
2026-02-04 12:48:29 +0700

    week01:(Tugas2) implement discount logic

--- src/oop_106784_ClaudiaVidya/week01/GameStore.kt
+++ src/oop_106784_ClaudiaVidya/week01/GameStore.kt
@@ -4,8 +4,12 @@ fun main() {
     val gameTitle = "Black Myth: Starlight"
     val price = 800000
 
+    val discount = calculateDiscount(price)
+
     println("Judul      : $gameTitle")
     println("Harga: Rp $price")
+    println("Diskon     : Rp $discount")
 
 }
 
+fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
