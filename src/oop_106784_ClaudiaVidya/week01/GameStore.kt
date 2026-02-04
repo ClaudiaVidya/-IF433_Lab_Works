857a693b05088ca64d6d954cd1bc7093ad0c06f6
Claudia <claudia.vidya@student.umn.ac.id>
2026-02-04 12:51:41 +0700

    week01:(Tugas2) implement receipt printing

--- src/oop_106784_ClaudiaVidya/week01/GameStore.kt
+++ src/oop_106784_ClaudiaVidya/week01/GameStore.kt
@@ -6,10 +6,16 @@ fun main() {
 
     val discount = calculateDiscount(price)
 
-    println("Judul      : $gameTitle")
-    println("Harga: Rp $price")
-    println("Diskon     : Rp $discount")
+    val finalPrice = price - discount
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== STRUK PEMBELIAN ===")
+    println("Judul Game : $title")
+    println("Total Bayar: Rp $finalPrice")
+    println("=======================")
+}
