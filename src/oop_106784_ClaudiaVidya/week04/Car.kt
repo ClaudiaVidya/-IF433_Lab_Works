71fb312aeebf315326b93ae8a38b158222d3ad53
Claudia <claudia.vidya@student.umn.ac.id>
2026-02-25 10:42:31 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_106784_ClaudiaVidya/week04/Car.kt
+++ src/oop_106784_ClaudiaVidya/week04/Car.kt
@@ -2,7 +2,7 @@ package oop_106784_ClaudiaVidya.week04
 
 open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
 
-    fun openTruck(){
+    fun openTrunk(){
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka");
     }
 
--- src/oop_106784_ClaudiaVidya/week04/Main.kt
+++ src/oop_106784_ClaudiaVidya/week04/Main.kt
@@ -8,7 +8,13 @@ fun main() {
 
     println("\n--- Testing Car ---")
     val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTruck();
+    myCar.openTrunk();
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val myEV = ElectricCar("Tesla Model 3", 4, 95)
+    myEV.openTrunk()
+    myEV.honk()
+    myEV.accelerate()
 }
