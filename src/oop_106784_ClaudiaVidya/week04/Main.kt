2683a7cf2a653fb245a74e49560cfbdd316c49fa
Claudia <claudia.vidya@student.umn.ac.id>
2026-02-25 10:20:58 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_106784_ClaudiaVidya/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_106784_ClaudiaVidya.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTruck();
+    myCar.honk()
+    myCar.accelerate()
+}
