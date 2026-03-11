c1ea0e07693eb042742f53428a7b4b7ea6343b82
Claudia <claudia.vidya@student.umn.ac.id>
2026-03-11 10:28:08 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_106784_ClaudiaVidya/week06/Smartphone.kt
+++ src/oop_106784_ClaudiaVidya/week06/Smartphone.kt
@@ -1,4 +1,9 @@
 package oop_106784_ClaudiaVidya.week06
 
 class Smartphone: Camera, Phone {
+    override fun turnon() {
+        super<Camera>.turnon()
+        super<Phone>.turnon()
+        println("Sistem operasi Smartphone berhasil shoooting...")
+    }
 }
