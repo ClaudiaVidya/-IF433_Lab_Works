4ec86db014682fded907168edef956d18f1493c7
Claudia <claudia.vidya@student.umn.ac.id>
2026-04-15 11:40:24 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_106784_ClaudiaVidya/week08/ApiParser.kt
@@ -0,0 +1,27 @@
+package oop_106784_ClaudiaVidya.week08
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        return try {
+            val id = requireNotNull(rawJson["id"] as? String) { "API Invalid: Missing ID" }
+            val name = requireNotNull(rawJson["name"] as? String) { "API Invalid: Missing Name" }
+
+            val type = rawJson["type"] as? String
+
+            when (type) {
+                "ELECTRONIC" -> {
+                    val warranty = rawJson["warranty"] as? Int ?: 12
+                    Electronic(id, name, warranty)
+                }
+                "CLOTHING" -> {
+                    val size = rawJson["size"] as? String ?: "All Size"
+                    Clothing(id, name, size)
+                }
+                else -> null
+            }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            null
+        }
+    }
+}
