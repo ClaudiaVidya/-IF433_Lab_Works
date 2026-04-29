3325938e805c563a3327f0c8ec6e850190c3a658
Claudia <claudia.vidya@student.umn.ac.id>
2026-04-29 10:20:50 +0700

    week10: (task) populate coin repository

--- src/oop_106784_ClaudiaVidya/week10/CryptoDashboard.kt
+++ src/oop_106784_ClaudiaVidya/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_106784_ClaudiaVidya.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 100.0))
+
 }
