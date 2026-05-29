adb4e65fd1d928b929556032ea1bac2712e915c6
Claudia <claudia.vidya@student.umn.ac.id>
2026-05-29 15:20:25 +0700

    week14: extract UserValidator and UserRepository

---
+++ src/oop_106784_ClaudiaVidya/week14/SrpRefactored.kt
@@ -0,0 +1,11 @@
+package oop_106784_ClaudiaVidya.week14
+
+class UserValidator {
+    fun validate(user: User): Boolean = user.email.contains("@") && user.age >= 18
+}
+
+class UserRepository {
+    fun save(user:User) {
+        println("Saving User: ${user.name} to database")
+    }
+}
